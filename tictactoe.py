39f3a3ba22807383c13934a2ac67f3d2bc87b960
F-Michelon <michelonfrancois00@gmail.com>
2023-12-08 15:06:33 +0100

    script v1

--- tictactoe.py
+++ tictactoe.py
@@ -14,15 +14,15 @@ def end_game(grille):
     else:
         # on test les lignes et les colonnes
         for i in range(3):
-            symbol_ligne = grille[i, 0]
-            symbol_colonne = grille[0, i]
+            symbol_ligne = grille[i, 0] * (grille[i, 0] != ' ') + '#' * (grille[i, 0] == ' ')
+            symbol_colonne = grille[0, i] * (grille[0, i] != ' ') + '#' * (grille[0, i] == ' ')
             ligne_complete = sum(grille[i, :] == symbol_ligne) == 3
             colonne_complete = sum(grille[:, i] == symbol_colonne) == 3 
             if ligne_complete or colonne_complete:
                 return True
         # on test les 2 diagonales
-        diag1 = sum(np.diag(grille) == grille[0, 0]) == 3
-        diag2 = sum(np.fliplr(grille).diagonal() == grille[2, 0]) == 3
+        diag1 = sum(np.diag(grille) == grille[0, 0] * (grille[0, 0] != ' ') + '#' * (grille[0, 0] == ' ')) == 3
+        diag2 = sum(np.fliplr(grille).diagonal() == grille[2, 0] * (grille[2, 0] != ' ') + '#' * (grille[2, 0] == ' ')) == 3
         if diag1 or diag2:      
             return True
             
@@ -43,7 +43,21 @@ def update_grille(grille=None, move=None):
                 grille[move[1], move[2]] = symbol
     return grille
 
-
-Grille = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
-Move = [0, 1, 1]
-print(update_grille(Grille, Move))
+def tictactoe():
+    ''' Fonction implémentant le jeu tictactoe à 2 joueurs
+    '''
+    grille = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
+    End_game = False
+    joueur = 0
+    while not End_game:
+        print(grille)
+        print("Choisir la ligne")
+        coordX = int(input())
+        print("Choisir la colonne")
+        coordY = int(input())
+        move = [joueur, coordX, coordY]
+        update_grille(grille, move)
+        End_game = end_game(grille)
+        joueur = 1 - joueur
+    print(grille)
+    print(f"Le joueur {1 - joueur} a gagné !!!")
