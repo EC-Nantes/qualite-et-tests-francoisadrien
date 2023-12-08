4989f00864200a47598182f3a45a97f0cd7a5ac8
F-Michelon <michelonfrancois00@gmail.com>
2023-12-08 14:51:32 +0100

    modif tictac

--- tictactoe.py
+++ tictactoe.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 def valide_move(grille=None, move=None):
-    if move is not None:
-        return grille[move[1], move[2]] == ''
+    ''' Renvoie True si le mouvement est valide et False sinon
+    '''
+    if move is not None and grille is not None:
+        return grille[move[1], move[2]] == ' '
     
 def end_game(grille):
-    if len(grille[grille != '']) == 9:
+    ''' Calcule si le jeu est fini ou non
+    '''
+    if len(grille[grille != ' ']) == 9:
         return True
     else:
         # on test les lignes et les colonnes
         for i in range(3):
             symbol_ligne = grille[i, 0]
             symbol_colonne = grille[0, i]
-            ligne_complete = sum(grille[grille[i, :] == symbol_ligne]) == 3
-            colonne_complete = sum(grille[grille[:, i] == symbol_colonne]) == 3 
+            ligne_complete = sum(grille[i, :] == symbol_ligne) == 3
+            colonne_complete = sum(grille[:, i] == symbol_colonne) == 3 
             if ligne_complete or colonne_complete:
                 return True
         # on test les 2 diagonales
@@ -28,19 +32,18 @@ def update_grille(grille=None, move=None):
     args:
         move : liste (int) de taille 3, move[0] = id joueur, move[1] = coordonnée x, move[2] = coordonnée y. 
     '''
-    
     if grille is None:
-        grille = np.array([['', '', ''], ['', '', ''], ['', '', '']])
+        grille = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
     else:
         if move is None:
             return grille
         else:
             symbol = 'X' * (move[0] == 1) + 'O' * (move[0] == 0)
-            if valide_move(move):
+            if valide_move(grille, move):
                 grille[move[1], move[2]] = symbol
     return grille
 
 
-Grille = np.array([['', '', ''], ['', '', ''], ['', '', '']])
+Grille = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
 Move = [0, 1, 1]
 print(update_grille(Grille, Move))
